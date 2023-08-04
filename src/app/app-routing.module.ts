8efc37e0a58d7fdb12e3dc971a0979d70f2f5370
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-04 17:08:53 -0500

    Creación maketación template crear Empleado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
 
-const routes: Routes = [];
+//Creamos nuestras rutas para poder movernos entre componentes o entre pantallas
+const routes: Routes = [
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full' }, //Ruta por defecto cuando la dirección es vacía
+  { path: 'list-empleados', component: ListEmpleadosComponent },
+  { path: 'create-empleado', component: CreateEmpleadoComponent },
+  { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' } //Ruta por defecto cuando se coloca cualquier cosa
+  //Se le puede colocar un componente que indique que la dirección no exíste y la opción a ir al inicio
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
