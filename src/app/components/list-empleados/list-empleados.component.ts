1b1273bd8c93f19fe058d3081a06a6092d382a49
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-08 18:07:41 -0500

    Lectura y muestra de datos en tabla

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -7,7 +7,9 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
   styleUrls: ['./list-empleados.component.css']
 })
 export class ListEmpleadosComponent implements OnInit {
-  empleados: any[] = []; // Para recoger los datos del Observable y luego pasarlo al html para mostrar
+
+  // Elemento para manipular los datos que vienen de la BD y acomodarlo a nuestro fin en mente.
+  empleados: any[] = []; // Para recoger los datos del Observable y luego pasarlo al html para mostrar en un ngFor.
 
   constructor(
     private empleadoService: EmpleadoService //Inyectamos el servicio
@@ -20,13 +22,14 @@ export class ListEmpleadosComponent implements OnInit {
   //Método para obtener los empleados de la BD
   getEmpleados() {
     this.empleadoService.obtenerEmpleados().subscribe(data => {
-      //console.log(data); //Nos muestra qué nos trae desde la base de Datos
       //Data nos trae un arreglo con el conjunto de elemento de tipo Object - Arreglo de Objetos
+      console.log(data); //Nos muestra qué nos trae desde la base de Datos - Los objetos en un arreglo
       //Podemos Manipular el arreglo y ser selectivos con los elemento usando ciclos de Js y condicionales
-      data.forEach((element:any) => {
-        //console.log(element.id); //En este caso estoy recogiendo sólo los id
-        console.log(element); //En este caso recogo todos los elementos - Objeto por Objeto
-      })
+      // data.forEach((element:any) => {
+      //   //console.log(element.id); //En este caso estoy recogiendo sólo los id
+      //   console.log(element); //En este caso recogo los elementos - Objeto por Objeto
+      // })
+      this.empleados = data; //Pasamos la información a una propiedad nativa de la clase
     })
   }
 
