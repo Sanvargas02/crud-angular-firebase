b776fa6bdaf8825c10aefc5f603302a651638408
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-08 10:57:00 -0500

    Validaciones y Formulario Reactivo Creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+//Firebase confi - Modulos Firebase
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
@@ -22,6 +28,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
--- src/app/components/create-empleado/create-empleado.component.ts
+++ src/app/components/create-empleado/create-empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-empleado',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-empleado.component.css']
 })
 export class CreateEmpleadoComponent {
+
+  createEmpleado: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.createEmpleado = fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      documento: ['', Validators.required],
+      salario: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  agregarEmpleado() {
+    this.submitted = true;
+    // Si el formulario de crear empleado es invalido (Cuando no se llenan los campos en este caso) se retorna y no ejecuta codigo más allá del if
+    if(this.createEmpleado.invalid) {
+      return ;
+    }
+    //Aquí podríamos usar una Interfaz
+    const empleado: any = {
+      nombre: this.createEmpleado.value.nombre,
+      apellido: this.createEmpleado.value.apellido,
+      documento: this.createEmpleado.value.documento,
+      salario: this.createEmpleado.value.salario,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    //empleado es el objeto que le vamos a pasar a firebase
+    console.log(empleado);
+  }
 
 }
