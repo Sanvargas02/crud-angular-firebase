eb168b3d9b68227cb8f3e6227cb6e3fb539889d5
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-09 17:57:49 -0500

    Empezando módulo de Edición

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,10 @@ const routes: Routes = [
   { path: '', redirectTo: 'list-empleados', pathMatch: 'full' }, //Ruta por defecto cuando la dirección es vacía
   { path: 'list-empleados', component: ListEmpleadosComponent },
   { path: 'create-empleado', component: CreateEmpleadoComponent },
+  { path: 'edit-empleado/:id', component: CreateEmpleadoComponent }, // El abutizo de: /:id -> significa que le vamos a pasar un parametro dinámico - También estamos reutilizando el componente CreateEmpleadoComponent.
+  //Esto lo hacemos para determinar en un mismo componente qué queremos hacer. En este caso Editar o Crear.
   { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' } //Ruta por defecto cuando se coloca cualquier cosa
-  //Se le puede colocar un componente que indique que la dirección no exíste y la opción a ir al inicio
+  //Se le puede colocar un componente que indique que la dirección no exíste y la opción a ir al inicio.
 ];
 
 @NgModule({
